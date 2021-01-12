b0818e4872cc5c484ff018b8685b813a9852962f
abcdlsj <lisongjianshuai@gmail.com>
2021-01-12 15:27:43 +0800

    complete BloomFilter(测试发现性能和别人的有差距，暂时不知道为什么，之后看看)

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # clsm
+
 C++ Log-Structured Merge-Tree (LSM-Tree)
+
+# Details
+
+## SkipList
+
+## BloomFilter 
+
+<https://findingprotopia.org/posts/how-to-write-a-bloom-filter-cpp/>
+
--- main.cpp
+++ main.cpp
@@ -1,18 +1,63 @@
 #include <iostream>
 
+#include "src/bloom_filter.hpp"
 #include "src/skip_list.hpp"
 
-int main() {
+using namespace std;
+
+struct timespec start, finish;
+
+void SkipListTest() {
   auto *skp = new SkipList<int, int>(1, 100);
   for (int i = 0; i < 100; i++) {
-    skp->InsertKey(i, i *100);
+    skp->InsertKey(i, i * 100);
   }
 
   for (int i = 0; i < 50; i++) {
     skp->DeleteKey(i);
   }
-  auto vec = skp->GetAll();
+  auto vec = skp->GetAllInRange(75, 100);
   for (auto &e : vec) {
     std::cout << e.key << " " << e.value << std::endl;
   }
 }
+
+void bloomFilterTest() {
+  std::random_device rand_dev;
+  std::mt19937 generator(rand_dev());
+  std::uniform_int_distribution<int> distribution(INT32_MIN, INT32_MAX);
+
+  const int num_inserts = 1000;
+  double fprate = .001;
+  BloomFilter<int32_t> bf = BloomFilter<int32_t>(num_inserts, fprate);
+
+  std::vector<int> to_insert;
+  for (int i = 0; i < num_inserts; i++) {
+    int insert = distribution(generator);
+    to_insert.push_back(insert);
+  }
+  clock_gettime(CLOCK_MONOTONIC, &start);
+  std::cout << "Starting inserts" << std::endl;
+  for (int i = 0; i < num_inserts; i++) {
+    bf.add(&i, sizeof(i));
+  }
+  clock_gettime(CLOCK_MONOTONIC, &finish);
+  double total_insert = (finish.tv_sec - start.tv_sec);
+  total_insert += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
+
+  std::cout << "Time: " << total_insert << " s" << std::endl;
+  std::cout << "Inserts per second: " << (int)num_inserts / total_insert << " s"
+            << std::endl;
+  int fp = 0;
+  for (int i = num_inserts; i < 2 * num_inserts; i++) {
+    bool lookup = bf.isContained(&i, sizeof(i));
+    if (lookup) {
+      // cout << i << "found but didn't exist" << endl;
+      fp++;
+    }
+  }
+  cout << fp << endl;
+  cout << "FP rate: " << ((double)fp / double(num_inserts)) << endl;
+}
+
+int main() { bloomFilterTest(); }
